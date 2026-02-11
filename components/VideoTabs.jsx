b7920ccba46b458678684ddaed8f02668f48e2ce
HemalStewart <nuwanhemal@gmail.com>
2026-02-11 09:22:31 +0530

    full width images one after another

--- components/VideoTabs.jsx
+++ components/VideoTabs.jsx
@@ -327,7 +327,7 @@ export default function VideoTabs({
                 }
 
                 return (
-                  <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+                  <div className="grid grid-cols-1 gap-6">
                     {materials.map((url, index) => (
                       <div
                         key={index}
